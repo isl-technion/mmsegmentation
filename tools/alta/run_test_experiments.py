6815611038c42bcc908e01be5f6b429892a48e20
Alon Gil-Ad <a10n@cs.technion.ac.il>
2022-08-29 17:16:55 +0300

    Small change

--- tools/alta/run_test_experiments.py
+++ tools/alta/run_test_experiments.py
@@ -4,8 +4,8 @@ import shutil
 import time
 from tools.alta.run_test_alta import main as mmseg_test
 
-# dest_dir = '/media/omek/Alta/experiments/20220717_144803'
 dest_dir = '/media/omek/Alta/experiments/arabella_test_post_sampling5/20220819_092331_equal'
+# dest_dir = '/media/omek/Alta/experiments/arabella_test_post_sampling5/20220823_094529_sqrt'
 
 trials_per_config = 1
 epoch_num = 320
@@ -14,9 +14,7 @@ cfg_options_list = ['data.test.separate_eval=1', 'data.test.separate_eval=0']
 
 train_val_spec_list = ['train_Agamim_All_val_IrYamim_Kikar']
 classes_type_list = ['all']  # 'all' \ 'noB' \ ?
-# model_type_list = ['segformer_mit-b3', 'segformer_mit-b0', 'deeplabv3plus_r50-d8', 'bisenetv2']  #  'bisenetv2' \ 'segformer_mit-b0\1\2' \ ...
-model_type_list = ['segformer_mit-b0', 'deeplabv3plus_r50-d8', 'deeplabv3plus_r18-d8']  # First GPU
-# model_type_list = ['segformer_mit-b3', 'bisenetv1_r50-d32', 'bisenetv1_r18-d32']  # Second GPU
+model_type_list = ['segformer_mit-b0', 'deeplabv3plus_r50-d8', 'deeplabv3plus_r18-d8', 'segformer_mit-b3', 'bisenetv1_r50-d32', 'bisenetv1_r18-d32']  # Second GPU
 weighting_method_list = ['equal']  # 'equal' \ 'sqrt' \ ?
 
 for train_val_spec in train_val_spec_list:
@@ -28,7 +26,7 @@ for train_val_spec in train_val_spec_list:
                     config_rel_path = os.path.join(train_val_spec, classes_type, model_type, weighting_method, trial_folder_name)
                     work_dir = os.path.join(dest_dir, config_rel_path)
                     config_file_path = os.path.join(work_dir, 'config.py')
-                    checkpoint_file_path = os.path.join(work_dir, 'iter_{}.pth'.format(epoch_num))
+                    checkpoint_file_path = os.path.join(work_dir, 'epoch_{}.pth'.format(epoch_num))
                     if not os.path.isfile(config_file_path):
                         print('Missing config file: ' + config_file_path)
                         continue
