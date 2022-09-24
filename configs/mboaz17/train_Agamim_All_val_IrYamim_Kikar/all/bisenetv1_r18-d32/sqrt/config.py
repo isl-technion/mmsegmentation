b9a57a2fef4b9ef5581b22b5d8b44268698d8c20
Alon Gil-Ad <a10n@cs.technion.ac.il>
2022-09-24 09:34:17 +0300

    All 12 config files for 'sqrt' method updated.

--- configs/mboaz17/train_Agamim_All_val_IrYamim_Kikar/all/bisenetv1_r18-d32/sqrt/config.py
+++ configs/mboaz17/train_Agamim_All_val_IrYamim_Kikar/all/bisenetv1_r18-d32/sqrt/config.py
@@ -14,26 +14,21 @@ _base_ = [
     '../../../schedule_320_epochs.py'
 ]
 
-num_classes=15
-AgamimPathA_hist = [383468, 227730863, 35295341, 18005882, 1173707, 4244303, 125600525, 13837139, 11737163, 5778866, 714936537, 234801492, 58013721, 464674200, 93447, 31522,]
-AgamimPathB_hist = [117317, 164123392, 57421339, 20429650, 105651, 5048636, 328455851, 21792463, 96998734, 1634637, 781109580, 473067269, 55777244, 409130156, 60616, 112041,]
-AgamimPathC_hist = [97745, 199139015, 24103206, 21193658, 94631, 4143944, 109727685, 18196612, 308767480, 916901, 663199290, 565340325, 36926677, 203529072, 404646, 99561,]
-Descend_hist = [4422502, 4882135267, 491870426, 338537919, 7635196, 66208360, 2813913853, 401135195, 1855076148, 17919689, 9781365668, 4204606894, 658432951, 6402717589, 6376412, 6615531,]  # without 0001, 0035, pre-sampled 1:5
-# class_weight = np.array([1/np.sqrt(AgamimPathA_hist[i]+AgamimPathB_hist[i]+AgamimPathC_hist[i]+Descend_hist[i]) for i in range(num_classes)])
-# class_weight /= class_weight.mean()
-class_weight = [3.88063889, 0.11753898, 0.35245326, 0.43577974, 2.89705656,
-       0.97436081, 0.14961989, 0.40767347, 0.18240645, 1.697205  ,
-       0.0795768 , 0.11748868, 0.30569269, 0.10054198, 3.30196682]
-# class_weight = [1.0 for i in class_weight]
+num_classes=16
+class_weight = [0., 4.86139222, 0.12775909, 0.29381101, 0.38981798, 2.55928649,
+                0.87541455, 0.16339358, 0.28703442, 0.1318935, 1.01571681, 0.09114451,
+                0.11215303, 0.33036596, 0.08321761, 3.67759923]
+
 crop_size = (1024, 1024)  # (5472, 3648)  # (1440, 1088)
 # stride_size = (768, 768)
+ignore_index=2
 
 # model settings
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
     # backbone=dict(init_cfg=dict(type='Pretrained', checkpoint=project_dir + 'pretrain/mit_b0.pth')),
     decode_head=dict(num_classes=num_classes,
-                     # ignore_index=1,
+                     # ignore_index=ignore_index,
                      loss_decode=dict(
                          type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0, class_weight=class_weight),  # , avg_non_ignore=True),
                      ),
@@ -44,7 +39,7 @@ model = dict(
             channels=64,
             num_convs=1,
             num_classes=num_classes,
-            # ignore_index=1,
+            # ignore_index=ignore_index,
             in_index=1,
             norm_cfg=norm_cfg,
             concat_input=False,
@@ -57,7 +52,7 @@ model = dict(
             channels=64,
             num_convs=1,
             num_classes=num_classes,
-            # ignore_index=1,
+            # ignore_index=ignore_index,
             in_index=2,
             norm_cfg=norm_cfg,
             concat_input=False,
@@ -75,7 +70,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='LoadAnnotations', reduce_zero_label=False),
     dict(type='Resize', keep_ratio=True, ratio_range=(0.85, 1.15)),  # img_scale=(2048, 1024)
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),  ###
     dict(type='RandomFlip', prob=0.5),
@@ -160,24 +155,24 @@ data = dict(
         data_root=data_root,
         img_dir=pathA_scenarios_img + pathB_scenarios_img + pathC_scenarios_img + Descend_scenarios_img,
         ann_dir=pathA_scenarios_ann + pathB_scenarios_ann + pathC_scenarios_ann + Descend_scenarios_ann,
-        reduce_zero_label=True,
-        # ignore_index=1,  # Ignoring buildings (after reducing the labels by 1)
+        reduce_zero_label=False,
+        # ignore_index=ignore_index,
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir=IrYamim_scenarios_img + PilotPath_img,
         ann_dir=IrYamim_scenarios_ann + PilotPath_ann,
-        reduce_zero_label=True,
-        # ignore_index=1,  # Ignoring buildings (after reducing the labels by 1)
+        reduce_zero_label=False,
+        # ignore_index=ignore_index,
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir=IrYamim_scenarios_img + PilotPath_img,
         ann_dir=IrYamim_scenarios_ann + PilotPath_ann,
-        reduce_zero_label=True,
-        # ignore_index=1,  # Ignoring buildings (after reducing the labels by 1)
+        reduce_zero_label=False,
+        # ignore_index=ignore_index,
         pipeline=test_pipeline))
 
 
